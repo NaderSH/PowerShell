ba8bd9d1ac409fe3dfa49a0c29fd405aa9a3488e
NaderSH <32811836+NaderSH@users.noreply.github.com>
2023-11-03 13:28:42 +0100

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,2 +1,4 @@
-# Environment Variables
+# 1- Environment Variables
 [array]$PodFunctionList = ("backup", "schema", "mssql", "scripts", "istio","monitoring")
+# 2- Functions Call
+.$FunctionsPath/Get-PortForwarding.ps1
