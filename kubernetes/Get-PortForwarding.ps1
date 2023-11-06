bedae1ddee8f5a17a168757aa7306456580c4303
NaderSH <32811836+NaderSH@users.noreply.github.com>
2023-11-06 13:12:00 +0100

    Update Get-PortForwarding.ps1

--- kubernetes/Get-PortForwarding.ps1
+++ kubernetes/Get-PortForwarding.ps1
@@ -6,7 +6,7 @@ Function Get-PortForwarding
     $output = New-Object System.Collections.Generic.List[Object]
     if((Get-ChildItem -Path Env: | Where-Object Name -eq "OS").Value -eq "Windows_NT"){
         Get-WmiObject Win32_Process | Where-Object CommandLine -like '*port-forward*' | Select-Object CreationDate, CommandLine | Sort-Object CreationDate -Desc | Format-List
-    }`
+    }
     else{
         ps -A -o pid,time,command | grep "port-forward" | sort -k 2 -r
     }
