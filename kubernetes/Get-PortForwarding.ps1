dd9dab33dd20f6e18b6ecb8551502726df96c7b4
NaderSH <32811836+NaderSH@users.noreply.github.com>
2023-11-06 13:11:13 +0100

    Update Get-PortForwarding.ps1

--- kubernetes/Get-PortForwarding.ps1
+++ kubernetes/Get-PortForwarding.ps1
@@ -1,23 +1,13 @@
-Function Get-PortForwarding()
+Function Get-PortForwarding
 {
+    param (
+    )
     # building output list    
     $output = New-Object System.Collections.Generic.List[Object]
     if((Get-ChildItem -Path Env: | Where-Object Name -eq "OS").Value -eq "Windows_NT"){
-        $processesList = Get-Process | Where-Object ProcessName -eq 'kubectl' | Select-Object Id, ProcessName, StartTime | Sort-Object StartTime -Desc
-        foreach ($process in $processesList) {
-            $tcpPort = Get-NetTCPConnection -State Listen | Where-Object {($_.OwningProcess -eq $process.Id) -AND ($_.LocalAddress -eq "127.0.0.1")}
-        # building output object
-        $outObj = [PSCustomObject]@{
-            StartTime    = $process.StartTime
-            LocalAddress = $tcpPort.LocalAddress
-            LocalPort    = $tcpPort.LocalPort
-            Process      = $tcpPort.OwningProcess
-            }
-        $output.Add($outObj)
-        }
-        $output | Sort-Object StartTime -Desc
+        Get-WmiObject Win32_Process | Where-Object CommandLine -like '*port-forward*' | Select-Object CreationDate, CommandLine | Sort-Object CreationDate -Desc | Format-List
     }`
     else{
-        ps -A -o pid,time,command | grep "kubectl port-forward" | sort -k 2 -r
+        ps -A -o pid,time,command | grep "port-forward" | sort -k 2 -r
     }
 }
