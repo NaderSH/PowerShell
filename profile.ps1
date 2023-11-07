cc567973ced09c8867ceaf63defcc7e6bbcf18c1
NaderSH <32811836+NaderSH@users.noreply.github.com>
2023-11-07 07:26:07 +0100

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,5 @@
 # 1- Environment Variables
-$FunctionsPath = 'The path where PowerShell functions are located on your local system.'
+[string]$FunctionsPath = 'The path where PowerShell functions are located on your local system.'
 [array]$PodFunctionList = ("backup", "schema", "mssql", "scripts", "istio","monitoring")
 # 2- Functions Call
 .$FunctionsPath/Get-PortForwarding.ps1
